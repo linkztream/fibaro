e1f3875c80eddf7d4f5b8e9ec80cdeb6d0a1e52e
Pär Lindström <meshwolf@users.noreply.github.com>
2015-07-15 12:51:53 +0200

    Fixade så att ljuset släcks också.

--- ljus_tvrum.lua
+++ ljus_tvrum.lua
@@ -12,12 +12,15 @@ end
 
 local isLampLit = fibaro:getValue(91, "value")
 local ampIsOn = fibaro:getGlobalValue("ampPower")
+local mSense = fibaro:getValue(55, "value")
 
-if (isLampLit == "0" and ampIsOn == "0") then
+if (isLampLit == "0" and ampIsOn == "0" and mSense == "1") then
    fibaro:call(91, "turnOn")
 elseif (isLampLit == "1" and ampIsOn == "1")
    then
    fibaro:call(91, "turnOff")
+elseif (isLampLit == "1" and ampIsOn == "0" and mSense == "0") then
+  fibaro:call(91,"turnOff")
 else
    fibaro:abort()
 end
