8069c22dc4622ba1ed348b699abe56b87195f949
Pär Lindström <meshwolf@users.noreply.github.com>
2015-08-05 16:08:57 +0200

    fixat konflikt

--- scheduler.lua
+++ scheduler.lua
@@ -4,7 +4,6 @@
 %% globals
 --]]
 
-<<<<<<< HEAD
 while true do
 
 if (fibaro:countScenes() > 1) then
@@ -67,26 +66,4 @@ executor(uBel, on, sunsetHour)
 executor(uBel, off, "22:00")
 
 
-=======
-local lightsOn = 1927
-local lightsOff = 1921
-local timer = 0
-while true do
-
-  local currentTime = tonumber(os.date("%H%M"))
-  
-  if (currentTime == lightsOn) then
-    fibaro:call(64, "pressButton", "1")
-    end
-  
-fibaro:setGlobal("ampPower", fibaro:getValue(4, "power"))
-fibaro:sleep(1000)
-timer = timer + 1
-
-
-if (timer >= 60) then
-   timer = 0
-end
-
->>>>>>> 41cad6d577cc471b137b3c80817cf502b9d6c6d5
 end
